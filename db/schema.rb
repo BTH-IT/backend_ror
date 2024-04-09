d8329af381131cbaf5c5fc714e1a236fa9ed6634
BTH-IT <hungbienthanh@gmail.com>
2024-04-10 01:04:19 +0700

    done

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_09_142723) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "weather_subscriptions", force: :cascade do |t|
     t.string "email", null: false
     t.string "location", null: false
