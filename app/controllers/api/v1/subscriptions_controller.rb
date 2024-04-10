6c7fce0da81c8450f7c2c54a3b99e2c9a0ca004b
BTH-IT <hungbienthanh@gmail.com>
2024-04-10 15:19:59 +0700

    fix bugs

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,12 +2,19 @@ class Api::V1::SubscriptionsController < ApplicationController
   def subscribe
     email = params[:email]
     location = params[:location]
+  
     if email.present? && location.present?
-      subscription = WeatherSubscription.subscribe(email, location)
-      if subscription
-        render json: { message: "Subscribed successfully" }
+      existing_subscription = WeatherSubscription.find_by(email: email)
+  
+      if existing_subscription
+        render json: { message: "Subscription exists for #{existing_subscription.email}" }, status: :bad_request
       else
-        render json: { error: "Subscription failed" }, status: :unprocessable_entity
+        subscription = WeatherSubscription.subscribe(email, location)
+        if subscription
+          render json: { message: "Subscribed successfully" }
+        else
+          render json: { error: "Subscription failed" }, status: :unprocessable_entity
+        end
       end
     else
       render json: { error: "Email and location cannot be blank" }, status: :unprocessable_entity
